22644729aa81e8608502f652c33226db3b10309a
Andrii Perevertailo <andrii.perevertailo@icloud.com>
2025-06-29 13:57:07 +0200

    Dodano requirements.txt oraz poprawiono NewCalculator.py

--- NewCalculator.py
+++ NewCalculator.py
@@ -1,26 +1,27 @@
+def oblicz(liczba1, operator, liczba2):
+    if operator == '+':
+        return liczba1 + liczba2
+    elif operator == '-':
+        return liczba1 - liczba2
+    elif operator == '*':
+        return liczba1 * liczba2
+    elif operator == '/':
+        if liczba2 != 0:
+            return liczba1 / liczba2
+        else:
+            raise ValueError("Dzielenie przez zero")
+    else:
+        raise ValueError("Nieprawidłowy operator")
+
+
 def kalkulator():
     print("Prosty kalkulator w Pythonie")
-    print("Dostępne operacje: +, -, *, /")
-
     liczba1 = float(input("Wprowadź pierwszą liczbę: "))
     operator = input("Wybierz operację (+, -, *, /): ")
     liczba2 = float(input("Wprowadź drugą liczbę: "))
-
-    if operator == '+':
-        wynik = liczba1 + liczba2
-    elif operator == '-':
-        wynik = liczba1 - liczba2
-    elif operator == '*':
-        wynik = liczba1 * liczba2
-    elif operator == '/':
-        if liczba2 != 0:
-            wynik = liczba1 / liczba2
-        else:
-            return print("Błąd: Dzielenie przez zero!")
-    else:
-        return print("Błąd: Nieprawidłowy operator!")
-
-    print(f"Wynik: {wynik}")
-
-kalkulator()
+    try:
+        wynik = oblicz(liczba1, operator, liczba2)
+        print(f"Wynik: {wynik}")
+    except ValueError as e:
+        print(f"Błąd: {e}")
 
